194cfc758bba800b68308e4b6183571648355ef9
Kage Edwards <kagemykel.edwards@gmail.com>
2026-09-10 00:13:02 -0700

    Remove unused function stubs from the local utils mod

--- adagrams/std_utils.py
+++ adagrams/std_utils.py
@@ -1,12 +1,5 @@
 from random import randint
-# Custom min() function
-def minimum(items: list) -> int | None:
-    todo("Implement min")
 
-# Custom max() function
-def maximum(items: list) -> int | None:
-    todo("Implement max")
-    
 def randindex(list: list) -> int:
     try:
         random_index = randint(0, len(list) - 1)
