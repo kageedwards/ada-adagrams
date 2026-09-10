8b8ce9758ece62c8a639519a083234641be9998a
Kage Edwards <kagemykel.edwards@gmail.com>
2026-09-10 00:50:04 -0700

    Document functions in game.py and std_utils.py

--- adagrams/game.py
+++ adagrams/game.py
@@ -1,5 +1,5 @@
 from random import randint
-from adagrams.std_utils import minimum, maximum, randindex, binary_search
+from adagrams.std_utils import randindex, binary_search
 
 LETTER_WEIGHTS = {
     "A": 9, "B": 2, "C": 2, "D": 4,
@@ -22,6 +22,7 @@ LETTER_SCORES = {
 }
 
 HAND_SIZE = 10
+BONUS_POINTS = 8
 
 def populate_letter_cache(letter_list: list):
     if not letter_list:
@@ -31,17 +32,23 @@ def populate_letter_cache(letter_list: list):
 
 def draw_letters() -> list[str]:
     hand = []
+    # Generate a simple list containing all the individual instances of
+    # available letters ( ["a", "a", "b", "c", "c", "d"] )
     letter_cache = []
     populate_letter_cache(letter_cache)
 
     for i in range(HAND_SIZE):
+        #select a random index
         random_letter_index = randindex(letter_cache)
+        # Fetch that letter, removing it from the list as we go
         random_letter = letter_cache.pop(random_letter_index)
+        # Add it to the hand
         hand.append(random_letter)
 
     return hand
 
 def uses_available_letters(word: str, letter_bank: list) -> bool:
+    # Let's not mess up the referenced letter_bank list
     letter_bank_tmp = letter_bank.copy()
     letter_bank_tmp.sort()
     for i in range(len(word)):
@@ -61,32 +68,42 @@ def uses_available_letters(word: str, letter_bank: list) -> bool:
 
 def score_word(word: str) -> int:
     total = 0
-    # We're only using latin alphabet characters
+    # note: this is problematic for future i18n
     cmp_word = word.upper()
     for i in range(len(cmp_word)):
         total += LETTER_SCORES[cmp_word[i]]
 
+    # this word gets a bonus :3
     if len(cmp_word) >= 7:
-        total += 8
+        total += BONUS_POINTS
 
     return total
 
 def get_highest_word_score(word_list: list) -> int | None:
+    # yay, sanity checks
     if not word_list:
         return None
-    
+
+    # score the first word
     highest_score = score_word(word_list[0])
     winning_word = word_list[0]
+
+    # algo to find the highest
     for i in range(1, len(word_list)):
         score = score_word(word_list[i])
         word = word_list[i]
+        # Score is higher. Simple.
         if score > highest_score:
             highest_score = score
             winning_word = word
+        # Tie breaking logic
         elif score == highest_score:
+            # Word over ten letters supercedes, 
+            # unless the previous candidate matches it
             if len(word) >= 10 and len(word) != len(winning_word):
                 highest_score = score
                 winning_word = word
+            # otherwise, thesmallest word takes precedence.
             elif len(word) < len(winning_word) < 10:
                 highest_score = score
                 winning_word = word
--- adagrams/std_utils.py
+++ adagrams/std_utils.py
@@ -7,18 +7,34 @@ def randindex(list: list) -> int:
     except ValueError as err:
         return 0
 
+# Binary search
+# Look in the middle.
+# - if it's our letter, return it,
+# - if it's not, move left and right
+# - based on which direction the letter's in.
+# Search gets smaller and smaller.
+# | --------------> mid <-------------- |
+# | ----> mid <---- |
+# | -mid- |
+# mid is needle. return mid
 def binary_search(haystack: list, needle: str) -> int | None:
     left = 0
     right = len(haystack)
 
     while(left < right):
+        # find the middle index of the list
         middle = (left + right) // 2
 
+        # if equal, we zeroed in on the letter. return the index
         if haystack[middle].casefold() == needle.casefold():
             return middle
+        # if letter comes after this item, move the left wall in
         elif haystack[middle].casefold() < needle.casefold():
             left = middle + 1
+        # if letter comes before this, move the right wall
+        # to our current middle
         elif haystack[middle].casefold() > needle.casefold():
             right = middle
 
+    # if nothing came up at all...
     return None
